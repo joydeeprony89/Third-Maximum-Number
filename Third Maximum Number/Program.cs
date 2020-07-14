0227700eaa80c8561cc1be2631caa6cc4b0587ae
joydeeprony89 <joydeeprony89@gmail.com>
2020-07-14 14:50:04 +0200

    Add project files.

---
+++ Third Maximum Number/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Third_Maximum_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2 };
+            Console.WriteLine(ThirdMax(arr));
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int ThirdMax(int[] nums)
+        {
+            // 1 , 11, -4, 0, 1, 5
+            int length = nums.Length;
+            int max = nums[0];
+            int secondMax = int.MinValue;
+            int thirdMax = int.MinValue;
+
+            for (int i = 0; i < length; i++)
+            {
+                int num = nums[i];
+                if(num > max)
+                {
+                    thirdMax = secondMax;
+                    secondMax = max;
+                    max = num;
+                } else if(num > secondMax && num < max)
+                {
+                    thirdMax = secondMax;
+                    secondMax = max;
+                } else if(num > thirdMax && num < secondMax)
+                {
+                    thirdMax = num;
+                }
+            }
+
+            return length > 2 ? thirdMax : max;
+        }
+    }
+}
